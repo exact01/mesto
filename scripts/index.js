51acda91d1cf74e36a94e7c2dd509b66e1164f1c
thechertan <[thechertan@yandex.ru]>
2021-10-02 03:18:29 +0300

    fix/bug index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,50 @@
 import initialCards from "./cards.js";
-
-//const popup = document.querySelector(".popup"); // Находим наш попапчик в dom дереве ;)
 const popupTypeEdit = document.querySelector(".popup_type_edit"); // select popup where type edit;
 const popupTypeAddcard = document.querySelector(".popup_type_add-card"); // select popup where type add-card
 const popupTypeImage = document.querySelector(".popup_type_image");
 const popupSrcImage = document.querySelector(".popup__image-full");
 const popupTextImage = document.querySelector(".popup__text");
-
 const popupEditBtn = document.querySelector(".profile__item-button-edit");
 const popupAddCard = document.querySelector(".profile__item-button-add");
-const popupCloseBtn = ".popup__close"; //close button
-
-//const formElement = document.querySelector(".popup__form");
+const popupCloseBtnOne = popupTypeEdit.querySelector(".popup__close_one");
+const popupCloseBtnTwo = popupTypeAddcard.querySelector(".popup__close_two");
+const popupCloseBtnThree = popupTypeImage.querySelector(".popup__close_three");
 const formElementEdit = document.querySelector(".popup__form_edit"); // for button edit
-const FormElementAddCard = document.querySelector(".popup__form_add-card"); // for button eddit card
-
+const formElementAddCard = document.querySelector(".popup__form_add-card"); // for button eddit card
 const nameInput = document.getElementById("form-title"); //  for profile
 const jobInput = document.getElementById("form-subtitle"); // for profile
-
 const inputLink = document.getElementById("form-link"); // for card
 const inputName = document.getElementById("form-name"); //for card
-
 const youName = document.querySelector(".profile__title");
 const youJob = document.querySelector(".profile__subtitle");
-
 const cardContainer = document.querySelector(".card-grid");
+
+formElementEdit.addEventListener("submit", formSubmitHandler); // for popupTypeEdit
+formElementAddCard.addEventListener("submit", formSubmitAddCard);
 
 popupEditBtn.addEventListener("click", () => {
   profileSubmitForm();
   openPopup(popupTypeEdit);
-  formElementEdit.addEventListener("submit", formSubmitHandler); // for popupTypeEdit
 });
 
 popupAddCard.addEventListener("click", () => {
   openPopup(popupTypeAddcard);
-  FormElementAddCard.addEventListener("submit", formSubmitAddCard);
+});
+
+popupCloseBtnOne.addEventListener("click", () => {
+  closePopup(popupTypeEdit);
+});
+
+popupCloseBtnTwo.addEventListener("click", () => {
+  closePopup(popupTypeAddcard);
+});
+
+popupCloseBtnThree.addEventListener("click", () => {
+  closePopup(popupTypeImage);
 });
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
-
-  popup
-    .querySelector(popupCloseBtn)
-    .addEventListener("click", () => closePopup(popup));
 }
 
 function closePopup(popup) {
@@ -53,16 +55,16 @@ function formSubmitAddCard(evt) {
   evt.preventDefault();
   const name = inputName.value;
   const link = inputLink.value;
-  FormElementAddCard.removeEventListener("submit", formSubmitAddCard);
   addCard(link, name, "prepend");
   closePopup(popupTypeAddcard);
+  inputName.value = "";
+  inputLink.value = "";
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   youName.textContent = nameInput.value;
   youJob.textContent = jobInput.value;
-  formElementEdit.removeEventListener("submit", formSubmitHandler); // delete even lister for clear memory
   closePopup(popupTypeEdit);
 }
 
@@ -88,18 +90,20 @@ function createCard(name, link) {
     cardElement.remove();
   });
   const popupOpenImage = cardElement.querySelector(".card-grid__image");
+
   popupOpenImage.addEventListener("click", function () {
     popupSrcImage.src = link;
+    popupSrcImage.alt = name;
     popupTextImage.textContent = name;
     openPopup(popupTypeImage);
   });
-
 
   const like = cardElement.querySelector(".card-grid__like");
   like.addEventListener("click", function (evt) {
     evt.target.classList.toggle("card-grid__like_activate");
   });
-  cardElement.querySelector(".card-grid__image").src = link;
+  popupOpenImage.src = link;
+  popupOpenImage.alt = name;
   cardElement.querySelector(".card-grid__subtitle").textContent = name;
   return cardElement; // back arr in a createCard
 }
